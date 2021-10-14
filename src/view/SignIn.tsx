fb18eae647f282ed682179ffb25c87b9b3dd3dde
lizhibo <lizhibo@zhihu.com>
2021-10-14 13:54:12 +0800

    add ico

--- src/view/SignIn.tsx
+++ src/view/SignIn.tsx
@@ -1,4 +1,4 @@
-import { createMuiTheme, Grid, makeStyles, MuiThemeProvider, Paper } from '@material-ui/core';
+import { createMuiTheme, Grid, Link, makeStyles, MuiThemeProvider, Paper } from '@material-ui/core';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
@@ -46,8 +46,8 @@ const SignInView = observer(() => {
           alignItems="center"
           className={classes.root}
         >
-          <Grid className={classes.paper} item xs={12} md={5} lg={4} xl={3} component={Paper} elevation={12} square>
-            <div>
+          <Grid className={classes.paper} item xs={12} md={5} lg={4} xl={3} component={Paper} elevation={12} square container direction="column" justify="space-between" alignItems="center">
+            <Grid>
               <Switch>
                 <Route path="/" exact>
                   <SignInForm />
@@ -59,7 +59,10 @@ const SignInView = observer(() => {
                   <ResetPasswordForm />
                 </Route>
               </Switch>
-            </div>
+            </Grid>
+            <Grid>
+              <Link color="textSecondary" variant="caption" href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">京ICP备2021025369号-1</Link>
+            </Grid>
           </Grid>
         </Grid>
       </MuiThemeProvider>
